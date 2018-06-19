b08ed3d3f5a41d2c39ca1b710171f2e662bc5f00
Tyler <backdoer@gmail.com>
2018-06-18 18:11:00 -0600

    change permissions after making files;

--- .make.sh
+++ .make.sh
@@ -30,13 +30,14 @@ for file in $files; do
     ln -s $dir/.$file ~/.$file
 done
 
-# Change permissions on autload file
-sudo chmod -R 755 ~/.vim/autoload
-
 # Install Vundle for managing Vim plugins
 echo "Installing Plug"
 sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Change permissions on autload file
+sudo chmod -R 755 ~/.vim/autoload
+
 
 # Install the rest of the plugins
 echo "Installing All Vim Plugins"
